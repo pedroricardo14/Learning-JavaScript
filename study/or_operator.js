8645b87b3e9a5faea7e44d72e0d9408b6072d87a
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-03-26 12:30:44 -0300

    Create new files.

---
+++ study/or_operator.js
@@ -0,0 +1,6 @@
+let isBatteryOn = true; 
+let isPowerOn = false; 
+console.log(isBatteryOn || isPowerOn);
+let isBatteryOn1 = false; 
+let isPowerOn1 = false; 
+console.log(isBatteryOn1 || isPowerOn1);
