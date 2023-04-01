d4efcd3758dd91a11f05045fec74d06e8ef85421
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-01 17:58:16 -0300

    Create condition.js

---
+++ study/condition.js
@@ -0,0 +1,55 @@
+let answer = "Picasso";
+if (answer === "Picasso") {
+ console.log(answer + " is correct!");
+}
+let answer1 = "Matisse";
+if (answer1 !== "Picasso") {
+ console.log(answer1 + " is wrong!");
+}
+let age = 75;
+if (age >= 55) {
+ console.log("Discount applied");
+}
+let isDay = true;
+if (isDay === true) {
+ console.log("Lights off!");
+}
+let score = 51;
+let pass = score > 50;
+if (pass) {
+ console.log(pass);
+}
+const song = "Don't stop me now";
+let replayTimes = 345;
+if (
+replayTimes >= 300) {
+ console.log("Your top song this week: ");
+ console.log(song);
+}
+const diet = "vegetarian";
+const veggieRestaurant = "Root";
+const restaurant = "DelPosto";
+if (diet === "vegetarian") {
+ console.log("Try out: ");
+ console.log(veggieRestaurant);
+}
+let today = "Sunday";
+if (today !== "Saturday") {
+ console.log("Set alarm at 8:00");
+}
+let age1 = 20;
+let canDrive = false;
+if (age1 > 18) {
+ canDrive = true;
+}
+console.log(canDrive);
+let inboxFull = true;
+const showAlert = inboxFull === true;
+if (showAlert) {
+ console.log("Your inbox is full.");
+ console.log("You can't receive any more emails");
+}
+let flightModeOn = true;
+if(flightModeOn === true) {
+ console.log("Turn flight mode off to connect to the internet");
+}
