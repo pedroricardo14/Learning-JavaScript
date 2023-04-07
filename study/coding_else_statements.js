e75615fd6883e2f4225366a023da781d689e45b0
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-07 16:33:12 -0300

    Create coding_else_statements.js

---
+++ study/coding_else_statements.js
@@ -0,0 +1,77 @@
+let condition = true;
+if (condition) {
+ console.log("1");
+}
+let condition1 = true;
+if (condition1) {
+ console.log("1");
+}if (!condition1) {
+ console.log("2");
+}
+let condition2 = false;
+if (condition2) {
+ console.log("1");
+} else {
+ console.log("2");
+}
+let isDay = false;
+if (isDay) {
+ console.log("Lights off!");
+} else {
+ console.log("Lights on!");
+}
+let number = 99;
+if (number === 1)
+ {
+ console.log("It's 1");
+} 
+else
+ {
+ console.log("It's not 1");
+}
+let isSubscribed = true;
+if (isSubscribed) {
+ console.log("Enjoy 10% off!");
+} else {
+ console.log("Become a subscriber!");
+}
+let chosenNumber = 7;
+if (chosenNumber === 12) {
+ console.log("You guessed right!");
+} else {
+ console.log("Have another go");
+}
+let commonFriends = 3;
+if (commonFriends > 2) {
+    console.log("Friend suggestions: Sue");
+} else {
+    console.log("No new friend suggestions");
+}
+let membership = "gold";
+if (membership === "gold") {
+ console.log("Members get a personal trainer");
+} else {
+ console.log("Members get a welcome drink")
+}
+let membership1 = "silver";
+if (membership1 === "gold") {
+ console.log("Get a personal trainer");
+} else {
+ console.log("Get a welcome drink");
+}
+let rating = 87;
+if (rating >= 90) {
+ console.log("Buy movie tickets");
+} else {
+ console.log("Homeflix & skip the bill");
+}
+let points = 
+7600
+;
+let pointsNeeded = 8000;
+if (points >= pointsNeeded) {
+ console.log("You're Level 2!");
+} else {
+ const left = pointsNeeded - points;
+ console.log("You need " + left + " points to get to Level 2");
+}
