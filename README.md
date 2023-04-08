2da67f65715461606e5070d29415aa0cba9066ae
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-08 20:42:08 -0300

    New codes

--- README.md
+++ README.md
@@ -13,4 +13,6 @@
 <li>logical operators</li>
 <li>Making decisions</li>
 <li>Using conditions</li>
+<li>Coding else statements</li>
+<li>incorporating else if</li>
 </ul>
---
+++ study/incorporating_else-if.js
@@ -0,0 +1,95 @@
+let hour = 9;
+if (hour < 12) {
+ console.log("Good morning");
+} else {
+ console.log("Good night");
+}
+
+let hour1 = 14;
+if (hour1 < 12) {
+ console.log("Good morning");
+} else if (hour1 < 17) {
+ console.log("Good afternoon");
+}
+
+let hour2 = 18;
+if (hour2 < 12) {
+ console.log("Good morning");
+} else if (hour2 < 17) {
+ console.log("Good afternoon");
+} else {
+ console.log("Good night");
+}
+
+let hour4 = 20;
+if (hour4 < 12) {
+ console.log("Good morning");
+} else if (hour4 < 17) {
+ console.log("Good afternoon");
+} else if (hour4 < 21) {
+ console.log("Good evening");
+} else {
+ console.log("Good night");
+}
+
+let number = 6;
+if (number < 5) {
+ console.log("Less than five");
+} else if (number < 10) {
+ console.log("Less than ten");
+}
+
+let score = 75;
+if (score > 70 && score < 90) {
+ console.log("You passed!");
+} else if (score >= 90) {
+ console.log("You get an A!");
+}
+
+let score1 = 75;
+if (score1 > 90) {
+ console.log("You get an A!");
+} else if (score1 > 80) {
+ console.log("You get a B!");
+} else {
+ console.log("Better luck next time!");
+}
+
+let age = 16;
+if (age <= 12) {
+ console.log("Where are your parents?");
+} else if (age < 17) {
+ console.log("You're too young to ride this ride.");
+}
+ else if (age >= 17) {
+ console.log("Welcome!");
+}
+
+let topping = "pepperoni";
+if (topping === "pineapple") {
+ console.log("Request denied.");
+} 
+else if
+ (topping === "pepperoni") {
+ console.log("Request accepted.");
+}
+
+let topping1 = "mushrooms";
+if (topping1 === "pineapple") {
+ console.log("Request denied.");
+} else if (topping1 === "pepperoni") {
+ console.log("Request accepted.");
+} else {
+ console.log("Could not process request.");
+}
+
+let age1 = 130;
+if (age1 < 12) {
+ console.log("Where are your parents?");
+} else if (age1 <= 15) {
+ console.log("You're too young to ride this ride.");
+} 
+else if
+ (age1 < 140) {
+ console.log("Welcome!");
+}
