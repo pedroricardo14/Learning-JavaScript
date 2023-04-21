c6a0ae92c4e8eb9e0add15c16a214ba2c6f127a0
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-21 17:35:53 -0300

    New file program

---
+++ study/Loops/assigningWithOperators.js
@@ -0,0 +1,39 @@
+let likes = 5;
+likes += 1;
+console.log(likes);
+
+let likes1 = 5;
+likes1 -= 3;
+console.log(likes1);
+
+let likes2 = 5;
+likes2 ++;
+console.log(likes2);
+
+let likes3 = 5;
+likes3 --;
+console.log(likes3);
+
+let marbles = 6;
+marbles += 2;
+console.log(marbles);
+
+let speed = 200;
+speed--;
+console.log(speed);
+
+let yards = 80;
+yards += 10;
+console.log(yards);
+
+let account = 1000;
+account -= 200;
+console.log(account);
+
+let wallet = 13;
+wallet ++;
+console.log(wallet);
+
+let wallet1 = 13;
+wallet1--;
+console.log(wallet1);
