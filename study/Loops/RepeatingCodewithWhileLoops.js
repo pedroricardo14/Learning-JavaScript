25ae88c64063e5beb9f5edf42faa646a94b891cf
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-23 16:50:05 -0300

    Add a while program

---
+++ study/Loops/RepeatingCodewithWhileLoops.js
@@ -0,0 +1,28 @@
+console.log("and again");
+console.log("and again");
+console.log("and again");
+console.log("and again");
+console.log("and again");
+
+// as //
+
+while
+ (true) {
+ console.log("and again");
+}
+
+// it's crashes the program 'cause it's a infinite loop
+
+while
+ (false) {
+ console.log("Skip me!");
+}
+
+while (true) {
+    console.log("Hi there!");
+}
+    
+while (true) {
+        console.log("To infinity,");
+        console.log("and Beyond!");
+}
