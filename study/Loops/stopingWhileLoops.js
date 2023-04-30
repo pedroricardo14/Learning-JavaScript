55c51a409cb37dcfb4ad2998b1e4b4d668bdcf6c
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2023-04-30 16:31:31 -0300

    create new programs

---
+++ study/Loops/stopingWhileLoops.js
@@ -0,0 +1,56 @@
+//let keepGoing = true;
+//while (keepGoing === true) {
+// console.log("and again");
+//}
+
+let keepGoing1 = true;
+while (keepGoing1 === true) {
+ console.log("and again");
+ keepGoing1 = false;
+}
+
+let keepGoing = true;
+while (keepGoing === true) {
+ console.log("keepGoing: " + keepGoing);
+ keepGoing = false;
+ console.log("keepGoing: " + keepGoing);
+}
+
+let isLoading = false;
+while (isLoading === true) {
+ console.log("Loading Game");
+}
+
+let keepGoing2 = true;
+while (keepGoing2 === true) {
+ console.log("Just keep swimming");
+ keepGoing2 = false;
+}
+
+let keepPlaying = true;
+while (keepPlaying === true) {
+ console.log("Now Playing: Dolce Vita");
+ keepPlaying = false;
+}
+
+let
+ 
+keepPlaying2 = true;
+while (keepPlaying2 === true) {
+ console.log("Now Playing: Ryan Paris");
+ keepPlaying2 = false;
+}
+
+let autopilotOn = false;
+while (
+autopilotOn
+ === true) {
+ console.log("Autopilot on: vroom!");
+ autopilotOn = false;
+}
+
+let keepPlaying3 = true;
+while (keepPlaying3 === true) {
+ console.log("Now Playing: Yummy");
+  keepPlaying3 = false;
+}
