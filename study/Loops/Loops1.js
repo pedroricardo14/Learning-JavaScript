8c285e93c6784da44f23d172e6fab5c610ba7bc7
pedroricardo14 <pedroricardo1032@gmail.com>
2023-05-05 21:30:18 -0300

    "New program"

---
+++ study/Loops/Loops1.js
@@ -0,0 +1,81 @@
+let lives = 2;
+lives = lives - 1;
+console.log("Lives left: " + lives);
+lives = lives - 1;
+console.log("Lives left: " + lives);
+console.log("Game over!");
+
+let launched = false;
+launched = launched;
+console.log(launched);
+
+let likes = 0;
+likes = likes + 1;
+console.log(likes);
+
+let list = "flour salt";
+list = list + " pepper";
+console.log(list);
+
+let wallet = 231;
+wallet -= 50;
+console.log(wallet);
+
+let welcome = "Welcome,";
+welcome += " Silvia";
+console.log(welcome);
+
+let temperature = 0;
+temperature -= 7;
+console.log(temperature);
+
+let stepCounter = 14000;
+stepCounter++;
+console.log(stepCounter);
+
+let hoursLeft = 30;
+hoursLeft--;
+console.log(hoursLeft);
+
+let isOn = true;
+
+/*
+let isOn = true;
+while (true) {
+ console.log("Warming up");
+}
+console.log("Cooling down");
+*/
+
+console.log("Let's go!")
+while (false) {
+ console.log("Repeat after me:");
+ console.log("I love to code!");
+}
+console.log("That's all!");
+
+let isLooping = true;
+while (isLooping === true) {
+ console.log("and again!");
+ isLooping = false;
+}
+
+let moreComments = true;
+while (moreComments === true) {
+ moreComments = false;
+ console.log("New comment:");
+ console.log("Maria: That website looks great!");
+}
+
+let isElectric = true;
+while (isElectric === true) {
+ console.log("Boogie woogie woogie");
+ isElectric = false;
+}
+
+let gameOn = false;
+while (gameOn === true) {
+ console.log("Play game music");
+ gameOn = false;
+}
+
