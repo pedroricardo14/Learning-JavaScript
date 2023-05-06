b92711d060ed2cd9fd8ff377ae3b51c8f0e0cf57
pedroricardo14 <pedroricardo1032@gmail.com>
2023-05-06 18:27:34 -0300

    Add new program

---
+++ study/Loops/controllingWhileLoops.js
@@ -0,0 +1,34 @@
+let counter = 1;
+while (counter < 4) {
+ console.log(counter); counter++;
+}
+
+let counter1 = 1;
+while (counter1 < 10) {
+ console.log(counter1);
+ counter1++;
+}
+
+let counter2 = 5;
+while (counter2 < 10) {
+ console.log(counter2);
+ counter2++;
+}
+
+let counter3 = 5;
+while (counter3 < 10) {
+    counter3++; 
+    console.log(counter3);
+}
+
+let speed = 2;
+while (speed < 5) {
+ console.log(speed);
+ speed++;
+}
+
+let listNumber = 1;
+while (listNumber < 11) {
+ console.log(listNumber + ". Camera");
+ listNumber++;
+}
