193701429a4b6cc145453c32c49d0efdf3fc9c02
pedroricardo14 <pedroricardo1032@gmail.com>
2023-05-08 17:07:47 -0300

    Create a new program

---
+++ study/Loops/repeatingCodesWithForLoops.js
@@ -0,0 +1,50 @@
+let counter1 = 0;
+while (counter1 < 5) {
+ console.log("**********---------");
+ counter1++;
+}
+let counter2 = 0;
+while (counter2 < 4) {
+ console.log("-------------------");
+ counter2++;
+}
+
+for (let i = 0; i < 4; i++) {
+    console.log("**********---------");
+   }for (let i = 0; i < 4; i++) {
+    console.log("-------------------");
+   }
+
+for (let i = 1; i < 5; 
+    i++
+    ) {
+    console.log(i);
+    }
+
+for (let i = 0; i < 2; i++) {
+    console.log("We will");
+       }
+    console.log("Rock you!");
+
+console.log("Everybody want to");
+for(let i = 0; i < 3; i++);{
+    console.log("be like me")
+}
+
+for (
+    let i = 5;
+     i < 10; i++) {
+     console.log(i);
+    }
+
+for (let i = 1; i < 5; i++) {
+    console.log("Round " + i);
+}
+
+for (let i = 50; i < 55; i++) {
+ console.log(i);
+}
+
+for (let index = 1; index < 5; index++) {
+    console.log(index);
+}
