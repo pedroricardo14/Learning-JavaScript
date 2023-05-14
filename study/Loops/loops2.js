037a8ddd9819593b56199aac7a3e362cf7210646
pedroricardo14 <pedroricardo1032@gmail.com>
2023-05-14 20:36:06 -0300

    Create Loops 2

---
+++ study/Loops/loops2.js
@@ -0,0 +1,67 @@
+let acceleration = 0;
+while (acceleration < 3) {
+ acceleration++;
+ console.log(acceleration);
+}
+
+let counter = 1;
+while (counter < 2) {
+ counter++;
+}
+console.log(counter);
+
+let tracker = 1;
+while (tracker <= 2) {
+ tracker++;
+}
+console.log(tracker);
+
+let counter1 = 0;
+while (counter1 < 3) {
+ console.log(counter1);
+ counter1++;
+}
+
+console.log("Fastest runners:");
+for (let index = 1; index < 4; index++) {
+ console.log(index + ".");
+}
+
+for (let i = 1; i < 5; i++) {
+    console.log(i); 
+}
+
+for (let i = 0; i < 3; i++) {
+    console.log("Polly wants a cracker!");
+}
+
+console.log("*");
+let line = "";
+for (let i = 1; i <= 5; i++) {
+ line += "~";
+ console.log(line);
+}
+
+let title = "Numbers:";
+let line1 = "";
+for (let i = 1; i < 6; i++) {
+ line1 += i;
+}
+console.log(line1);
+
+for (let i = 5; i >= 0; i--) {
+    console.log(i);
+}
+
+for (let i = 10; i > 5; i--) {
+    console.log(i);
+}
+
+for (let i = 30; i > 25; i--) {
+    console.log(i);
+}
+
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
