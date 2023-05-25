d7541d7b87e9e86b4ae5c2bb1814ac9e7993c8a9
pedroricardo14 <pedroricardo1032@gmail.com>
2023-05-25 19:00:31 -0300

    Create a new program

---
+++ study/arrays/changingValuesInArrays.js
@@ -0,0 +1,32 @@
+const temperatures = [17, 20, 26, 24];
+console.log(temperatures);
+
+const temperatures1 = [17, 20, 26, 24];
+console.log(temperatures1[1]);
+
+const temperatures2 = [17, 20, 26, 24];
+console.log(temperatures2[2]);
+
+const temperatures3 = [17, 20, 26, 24];
+temperatures3[2] = 25;
+console.log(temperatures3);
+
+const temperatures4 = [17, 25, 26, 24];
+temperatures4[3] = 20;
+console.log(temperatures4);
+
+const animals = ["cat", "dog"];
+console.log(animals);
+
+const lockCombination = [9, 4, 7];
+console.log(lockCombination);
+
+const items = ["milk", "tomato", "apple"];
+console.log(items[1]);
+
+const even = [3, 4, 6];
+even[0] = 2;
+console.log(even);
+
+const topSpeeds = [100, 130, 240];
+console.log(topSpeeds[2]);
